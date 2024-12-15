6aa4d05b0d58ae1fa999a10793f917e761e06100
akixxxxx <akixxxxx01@gmail.com>
2024-12-15 18:32:16 +0900

    エラーメッセージの重複の調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nick_name, presence: true
   validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は6文字以上で英数字をそれぞれ含める必要があります' },
-                       confirmation: true
-  validates :encrypted_password, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 6, message: 'は6文字以上で入力してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英数字をそれぞれ含める必要があります' }
+  validates :password, confirmation: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :given_name
