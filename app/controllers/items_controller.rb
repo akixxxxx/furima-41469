e6a899f873b2dd260453c41d78fa6ac2ccd42b9b
akixxxxx <akixxxxx01@gmail.com>
2024-12-22 13:18:37 +0900

    商品投稿機能の実装（写真の投稿以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index
     @item = Item.all
   end
+
+  def new
+    @item = Item.all
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :users
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_information
+  belongs_to :product_condition_information
+  belongs_to :shipping_cost_information
+  belongs_to :shipping_origin_information
+  belongs_to :shipping_time
+
+  # ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_information_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :product_condition_information_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_information_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_origin_information_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_time_id, numericality: { other_than: 1, message: "can't be blank" }
 end
