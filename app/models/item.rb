e1e1538f06fdd9c0945e2c96a8eadc6610db992c
akixxxxx <akixxxxx01@gmail.com>
2024-12-22 16:15:17 +0900

    ログアウト状態から出品しようとするとログインページに飛ぶ

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_origin_information, class_name: 'ShippingOriginInformation'
   belongs_to :shipping_time, class_name: 'ShippingTime'
 
+  validates :pricing_information,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'must be between ¥300 and ¥9,999,999' }
+
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_information_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :product_condition_information_id, numericality: { other_than: 1, message: "can't be blank" }
