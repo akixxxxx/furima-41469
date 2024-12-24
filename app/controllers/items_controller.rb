800f1e16d611742496bf179f1783216196044c7b
akixxxxx <akixxxxx01@gmail.com>
2024-12-24 21:58:47 +0900

    一覧表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
